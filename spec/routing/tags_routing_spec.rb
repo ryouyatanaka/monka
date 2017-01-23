70b11cc63d46cb6c52a922a8016f59a69d4cca61
ryouya tanaka <ryouramurakiri@gmail.com>
2017-01-23 08:53:25 +0000

    ルーティングテスト

--- spec/routing/tags_routing_spec.rb
+++ spec/routing/tags_routing_spec.rb
@@ -11,10 +11,6 @@ RSpec.describe TagsController, type: :routing do
       expect(:get => "/tags/new").to route_to("tags#new")
     end
 
-    it "routes to #show" do
-      expect(:get => "/tags/1").to route_to("tags#show", :id => "1")
-    end
-
     it "routes to #edit" do
       expect(:get => "/tags/1/edit").to route_to("tags#edit", :id => "1")
     end
