29bf7fa0d2d95f83e6591d931400999c8d87e0f1
ryouya tanaka <ryouramurakiri@gmail.com>
2017-01-23 09:09:35 +0000

    表示・非表示のテストコード

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-
+  describe "scope visible" do
+    let(:show_book) { Book.create(title: 'New Ruby3.0', author: 'Matz', published_on: Time.zone.now, showing: true) }
+    let(:hide_book) { Book.create(title: 'New Ruby2.0', author: 'Matz', published_on: 1.year.ago, showing: false) }
+    
+    it { expect(Book.visible).to include show_book }
+    it { expect(Book.visible).not_to include hide_book }
+  end
 end
