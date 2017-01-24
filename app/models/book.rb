bfa96a9575e368efa2eef29435b6b4e471cf5639
ryouya tanaka <ryouramurakiri@gmail.com>
2017-01-24 01:05:46 +0000

    tagging relation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
-    
+    has_many :taggings
+    has_many :tags, through: :taggings
     scope :visible, ->(){ where(showing: true) }
-end 
+end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
-
-end
+  has_many :taggings
+  has_many :books, through: :taggings
+  end
