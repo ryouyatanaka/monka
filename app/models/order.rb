9117781a80c7694f069802c325fb442fb091b97c
ryouya tanaka <ryouramurakiri@gmail.com>
2017-01-25 04:24:41 +0000

    routsの変更

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_one :order_detail
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :orders
+  resources :orders, only: [:new, :create]
   resources :musics
   get 'products/index'
   root to: 'products#index'
